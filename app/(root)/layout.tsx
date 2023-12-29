41f1b68c9ce3e3485d7fc931b67a5f7ec693ec4f
karmalor <karmalor@yahoo.com>
2023-12-29 10:11:53 -0700

    first coommit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
